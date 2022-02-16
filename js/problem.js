36e5e2954316968e3e069f1b3aac7a20453d7c39
heishi1HUMANITY <i.wanna.make.this.main@gmail.com>
2022-02-16 18:02:31 +0900

    大西:コードの追加

---
+++ js/problem.js
@@ -0,0 +1,348 @@
+const recipe = [
+  {
+    FoodName: "kor moo yang",
+    Ingredients: [
+      "oyster sauce",
+      "soy sauce",
+      "black soy sauce",
+      "ground white pepper",
+      "water",
+      "pork neck"
+    ],
+    Recipe: [
+      "In a large bowl, combine the oyster sauce, soy sauce, black soy sauce, ground white pepper and water to make a marinade.",
+      "Add the pork neck to the bowl. Take a fork and prick some holes into it so that the pork will absorb the marinade.",
+      "Cover the bowl with food wrap and marinate in the refrigerator for 30 minutes.",
+      "After 30 minutes, turn your convection oven on at 437°F or 225°C. Place the pork on a grilling rack and cook for 30 minutes, turning the other side up halfway through.",
+      "Turn off the oven. Transfer the pork to a cutting board and slice to serve with steamed rice or sticky rice, nam jim jaew and fresh vegetables."
+    ]
+  },
+  {
+    FoodName: "Chicken and Basil Rice with Fried Egg",
+    Ingredients: [
+      "uncooked jasmine rice",
+      "oyster sauce",
+      "fish sauce",
+      "sugar",
+      "vegetable or canola oil",
+      "cloves of garlic",
+      "serrano peppers",
+      "boneless chicken breast",
+      "red bell pepper",
+      "white onion",
+      "eggs",
+      "salt",
+      "black pepper",
+      "Thai basil",
+      "cilantro"
+    ],
+    Recipe: [
+      "Cook the rice. Bring 4 cups of water to a boil. Put in the rice, cover the pot and let it cook for about 20 to 25 minutes, or until all of the water is absorbed. While the rice is cooking, do not remove the lid. Taking off the lid lets out the steam, causing the rice to cook improperly.",
+      "Prepare the other parts of the dish while the rice is cooking.\n3. In a small bowl, whisk the eggs, salt, and black pepper. Set it aside.",
+      "In a small bowl, whisk together the oyster sauce, fish sauce, and sugar. Set it aside.",
+      "Heat the oil in a wok until the oil begins letting off smoke. Add the garlic and serrano peppers and stir.",
+      "Add the red bell pepper and onion and saute until they become tender. Add the chicken and cook until it is no longer pink.",
+      "Push the veggies and chicken to the sides of the wok. Pour the eggs into the middle and cook until no longer wet.",
+      "Stir in the oyster sauce mixture and saute until all of the ingredients are evenly coated.",
+      "Add the cooked rice to the skillet. Stir and cook until all of the ingredients are well combined, about five minutes.",
+      "Remove the wok from the heat.",
+      "Add the chopped basil and top with cilantro.",
+      "Serve."
+    ]
+  },
+  {
+    FoodName: "Pad Thai",
+    Ingredients: [
+      "onion",
+      "garlic cloves",
+      "chicken breast",
+      "eggs",
+      "beansprouts",
+      "firm tofu",
+      "garlic chives",
+      "peanuts",
+      "Chang’s Pad Thai dried rice sticks",
+      "tamarind puree",
+      "brown sugar",
+      "fish sauce",
+      "oyster sauce"
+    ],
+    Recipe: [
+      "Place noodles in a large bowl, pour over plenty of boiling water. Soak for 5 minutes, then drain in a colander and quickly rinse under cold water. Don’t leave them sitting around for more than 5 – 10 minutes.",
+      "Mix Sauce in small bowl.",
+      "Heat 2 tbsp oil in a large non stick pan (or well seasoned skillet) over high heat. Add garlic and onion, cook for 30 seconds.",
+      "Add chicken and cook for 1 1/2 minutes until mostly cooked through.",
+      "Push to one side of the pan, pour egg in on the other side. Scramble using the wooden spoon (add touch of extra oil if pan is too dry), then mix into chicken.",
+      "Add bean sprouts, tofu, noodles then Sauce.",
+      "Toss gently for about 1 1/2 minutes until Sauce is absorbed by the noodles.",
+      "Add garlic chives and half the peanuts. Toss through quickly then remove from heat.",
+      "Serve immediately, sprinkled with remaining peanuts and lime wedges on the side, with a sprinkle of chilli and a handful of extra beansprouts on the side if desired (this is the Thai way!). Squeeze over lime juice to taste before eating."
+    ]
+  },
+  {
+    FoodName: "Khao Neo Mamuang",
+    Ingredients: [
+      "sticky rice",
+      "mangoes",
+      "pandan leaves",
+      "coconut milk",
+      "sugar",
+      "salt",
+      "corn starch",
+      "toasted white sesame"
+    ],
+    Recipe: [
+      "Bring a pot of water to a hard boil. Put a steamer on top of the pot and steam the rice that has been soaked overnight in a cheesecloth in a steamer with a lid on for 30 minutes.",
+      "While the sticky rice is cooking, make the coconut syrup for sweetening the rice by bringing 1 cup of coconut milk, ¼ teaspoon of salt, ½ cup plus 2 tablespoons of granulated sugar and 3 pandan leaves to a gentle boil.",
+      "Make the coconut sauce for drizzling over the sweet sticky rice by bringing ¾ cup of coconut milk and ¼ teaspoon of salt to a gentle boil. Then, add 1 tablespoon of corn starch, stir until thickened and remove from heat.",
+      "Once the sticky rice is cooked, transfer it from the cheesecloth to a pot.",
+      "Add the coconut syrup to the rice, mix it in with a spatula. Then, cover the pot with a lid and let the rice absorb the coconut syrup for 20-30 minutes. The sweet sticky rice is ready when the rice has absorbed all the liquid and the rice should look shiny.",
+      "Prepare the mangoes by slicing them lengthways on both sides of the stones. Then score a lattice into the flesh without cutting through the skin and gently push the skin to make the cubes stand out. Alternatively, you can peel the mangoes and cut the flesh into bite-size pieces.",
+      "To serve, arrange the mango and sweet sticky rice on a serving plate side by side. You might want to garnish with fresh pandan or mint leaves just to add a bit of green to the plate.",
+      "Drizzle the salty coconut sauce over the sticky rice and top the sauce with toasted white sesame seeds or fried split mung beans and serve."
+    ]
+  },
+  {
+    FoodName: "Khao Man Gai",
+    Ingredients: [
+      "whole chicken",
+      "salt",
+      "cilantro roots",
+      "Thai garlic",
+      "Thai jasmine rice",
+      "winter melon",
+      "black pepper",
+      "cilantro",
+      "ginger",
+      "garlic",
+      "Thai bird's eye chiles",
+      "fermented soybean sauce",
+      "white vineger",
+      "suger",
+      "dark soy sauce",
+      "chicken broth",
+      "cucumber"
+    ],
+    Recipe: [
+      "Remove the chicken skin and set aside to make chicken oil later.",
+      "In a large pot, add the chicken and cover with enough water so that the chicken is completely submerged. Add the salt and coriander roots into the water. Bring your pot to high heat.",
+      "Once the water is boiling, reduce to low heat. Cook the chicken for 30 to 40 minutes.",
+      "Check to see if your chicken is cooked, and remove it from the broth to cool on the side.",
+      "Bring a small pan to medium heat and add the chicken skin.",
+      "Cook the chicken skin until all the oil is released. Cook for 10 minutes and remove the chicken skin. Turn the heat on low.",
+      "Add the chopped garlic to the hot oil, and let it fry in the pan. Continually stir the garlic so that it doesn’t stick to the pan. Cook for 5 minutes.",
+      "Once the garlic is a golden brown, put it in a bowl along with the chicken oil it cooked in.",
+      "In an medium sized pot, rinse your rice with water until the water is clear. Drain the water from the rice.",
+      "Add the fried garlic and chicken oil to the rice and mix well.",
+      "Pour in chicken broth reserved from making the chicken until it covers the rice. You can measure if you have enough broth by putting your index finger to touch the top of the rice. The broth should reach your first joint line on your finger.",
+      "Add salt to taste and mix into the rice.",
+      "When the rice comes to a boil, reduce the heat to low and cover the pot with the lid.",
+      "Cook for 20 minutes and check on your rice. When the rice is finished cooking, open the cover and fluff it up with a fork.",
+      "Peel the winter melon and remove the seeds. Cut the winter melon into 2-inch pieces.",
+      "Bring your chicken broth to a boil and add the winter melon.",
+      "Season with salt and pepper to taste.",
+      "Cook the winter melon for 15 minutes until soft.",
+      "Turn off the heat and add the chopped cilantro.",
+      "Put the ginger, garlic, chile, and cilantro root into a blender and pulse until they are roughly chopped.",
+      "Add the fermented soybean sauce, white vinegar, sugar, dark soy sauce, and chicken broth into the mixture. Blend together, and keep little chunks for texture.",
+      "Using a small soup bowl, fill it with the cooked rice, pat it down, then flip it over onto a plate to create a nice round mound of rice.",
+      "Next, using a carving knife or a chef's knife, carve the cooked chicken; slicing the breast meat.",
+      "Place the sliced breast meat on top of the rice mound, garnishing it with sliced cucumber and cilantro. Serve with a small bowl of the soup and another small dish filled with the sauce."
+    ]
+  },
+  {
+    FoodName: "Som Tam",
+    Ingredients: [
+      "papaya",
+      "green bean",
+      "chilli pepper",
+      "garlic",
+      "lime",
+      "fish sauce",
+      "sugar",
+      "peanut",
+      "tomato",
+      "dried shrimp"
+    ],
+    Recipe: [
+      "Peel and seed papaya and shred it. Put in a bowl.",
+      "Add chilli pepper, garlic, lime, fish sauce and suger. Crush with a pestle."
+    ]
+  },
+  {
+    FoodName: "Tom Yum Goong",
+    Ingredients: [
+      "shrimp",
+      "chicken stock",
+      "galangal",
+      "lemongrass",
+      "kaffir lime leave",
+      "mushroom",
+      "chilli pepper",
+      "lime juice",
+      "fish sauce"
+    ],
+    Recipe: [
+      "Heat the chicken stock and when it boils, add the galangal, lemongrass and kaffir lime leave.",
+      "When it starts to smell, add mushroom and shrimp.",
+      "When the color of the shrimp changes, season with the chilli pepper, nam phrik phao, lime juice and fish sauce."
+    ]
+  },
+  {
+    FoodName: "Green Curry",
+    Ingredients: [
+      "chicken",
+      "eggplant",
+      "red bell pepper",
+      "green pepper",
+      "mushroom",
+      "coconut milk",
+      "green curry paste",
+      "sugar",
+      "fish sauce",
+      "vegetable oil"
+    ],
+    Recipe: [
+      "Cut the eggplant into large chunks. Separate the mushroom.",
+      "Cut the red bell pepper and green pepper into 1cm pieces.",
+      "Cut the chiken into bite size pieces.",
+      "Put the vegetable into the flying pan heated on medium heat, add chicken and stir-fry.When the color changes, add eggplant and mushroom and stir-fry.When all are coated, add green curry paste and stir-fly until all ingridients are well mixed.",
+      "Add the coconut milk and simmer on low heat for 15 minutes.When the chicken is cooked, stir in sugar and fish sauce and simmer on low heat for 5 minutes, then remove from heat."
+    ]
+  },
+  {
+    FoodName: "Massaman Curry",
+    Ingredients: [
+      "coconut milk",
+      "onion",
+      "peanut",
+      "cashews",
+      "potato",
+      "bay leave",
+      "star anise",
+      "sugar",
+      "fish sauce",
+      "chilli pepper",
+      "tamarind sauce"
+    ],
+    Recipe: [
+      ""
+    ]
+  },
+  {
+    FoodName: "Stir Fried Chicken",
+    Ingredients: [
+      "chicken",
+      "cornstarch",
+      "chilli pepper",
+      "onion",
+      "cashew",
+      "garlic",
+      "green onion",
+      "fish sauce",
+      "sugar",
+      "chilli sauce",
+      "pepper",
+      "vegetable oil"
+    ],
+    Recipe: [
+      "Mix fish sauce, sugar, ketchup, chilli sauce and pepper.",
+      "Sprinkle cornstarch on chicken.Fry chilli pepper, chicken and cashwe in hot vegetable oil, then move to plate.",
+      "Stir-fry the garlic in a flying pan until aromatic, then add the onion and stir.",
+      "Add first step ingredients and stir until sticky, then add second step ingredients and green onion and stir."
+    ]
+  }
+];
+
+let tmp = [];
+recipe.forEach(v => {
+  tmp.push(...v.Ingredients);
+});
+const ingredients = Array.from(new Set(tmp));
+
+tmp = [];
+recipe.forEach(v => {
+  tmp.push(v.Ingredients);
+});
+const ingredientsBits = tmp.map(v => ingredients.map(i => v.find(g => g == i) ? 1 : 0));
+
+const createQuestion = () => recipe[Math.floor(Math.random() * recipe.length)].FoodName;
+
+const createSelector = (name) => {
+  const bit = ingredientsBits[recipe.findIndex(r => r.FoodName === name)];
+  let randomSelect = recipe[recipe.findIndex(r => r.FoodName === name)].Ingredients.length * 2 - recipe[recipe.findIndex(r => r.FoodName === name)].Ingredients.length;
+  return bit.map(b => {
+    if (b === 1 || randomSelect < recipe[recipe.findIndex(r => r.FoodName === name)].Ingredients.length * 2) {
+      randomSelect++;
+      return 1;
+    }
+    return 0;
+  });
+}
+
+const checkDistance = (answer) => {
+  const answerBit = ingredients.map(v => answer.find(a => a === v) ? 1 : 0);
+  return ingredientsBits.map(v => {
+    let tmp = 0;
+    for (let i = 0; i < answerBit.length; i++) {
+      tmp += Math.pow(answerBit[i] - v[i], 2);
+    }
+    return Math.sqrt(tmp);
+  });
+};
+
+const checkScore = (answer, food) => {
+  const ingredients = recipe[recipe.findIndex(v => v.FoodName === food)].Ingredients;
+  console.log(ingredients);
+  console.log(answer)
+  let num = 0;
+  answer.forEach(v => ingredients.find(i => i == v) ? num++ : 0);
+  return num / ingredients.length;
+};
+
+const questionDom = document.querySelector('#question');
+const selector = document.querySelector('#selector');
+const submit = document.querySelector('#submit');
+const result = document.querySelector('#result');
+
+const appendQuestion = () => {
+  const p = document.createElement('p');
+  p.textContent = createQuestion();
+  questionDom.appendChild(p);
+  return p.textContent;
+};
+
+const appendSelector = (food) => {
+  const bit = createSelector(food);
+  for (let i = 0; i < bit.length; i++) {
+    if (bit[i] === 1) {
+      const label = document.createElement('label');
+      label.textContent = ingredients[i];
+      const input = document.createElement('input');
+      input.setAttribute('type', 'checkbox');
+      input.setAttribute('value', ingredients[i]);
+      label.appendChild(input);
+      selector.appendChild(label);
+    }
+  }
+};
+
+const food = appendQuestion();
+appendSelector(food);
+
+submit.addEventListener('click', () => {
+  const input = document.querySelectorAll('input');
+  const tmp = [];
+  for (const i of input) {
+    if (i.checked) tmp.push(i.value);
+  }
+  const distance = checkDistance(tmp);
+  
+  const score = checkScore(tmp, food);
+
+  const p = document.createElement('p');
+  p.textContent = score.toString();
+
+  result.appendChild(p);
+
+});
