bcb8c08022c17608d7932fee4f9c1cdf3d4b7afa
nescal29 <taisuke0129t@gmail.com>
2022-02-17 10:41:04 +0900

    Delete not need file and project-dir

--- cook_app/recipe_serch/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RecipeSerchConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'recipe_serch'
--- cook_app/recipe_serch/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-def test_view(request):
-  params = {
-    'page_title': 'Test Page.',
-    'message': 'This page for testing.'
-  }
-
-  return render(request, 'testpage.html', params)
